509b8ba151ed1a7b754a357306e9bb376fec34a7
Raissa Hohenester <raissa.hohenester@pentlandfirth.com>
2019-07-29 13:50:25 +0200

    Updates

---
+++ precache-manifest.890ab640532a506025d19112364a6cfc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f099733933d6632b6542f8bc75dbfd0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b14cfdd1b6484f3126e3",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "785d040c656939865d4b",
+    "url": "/static/js/2.50c33f98.chunk.js"
+  },
+  {
+    "revision": "b14cfdd1b6484f3126e3",
+    "url": "/static/js/main.7b9c776d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
